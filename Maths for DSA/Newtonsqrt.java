d003b17a0a290dea1d4ca903c32008904f56109a
ronitsharma03 <ronitkhajuria03>
2024-03-15 19:57:22 +0530

    Adding codes for some java

---
+++ Maths for DSA/Newtonsqrt.java
@@ -0,0 +1,18 @@
+public class Newtonsqrt {
+    public static void main(String[] args){
+        System.out.println(newtonSqrt(36));
+    }
+    static double newtonSqrt(double n){
+        double x = n;
+        double root;
+        while(true){
+            root = 0.5 * (x + (n/x));
+
+            if(Math.abs(root - x) < 0.5){
+                break;
+            }
+            x = root;
+        }
+        return root;
+    }
+}
