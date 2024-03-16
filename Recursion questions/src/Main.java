62a1acc254d2820f1c0ab9bdb1b4fabef5c5b08b
ronitsharma03 <ronitkhajuria03>
2024-03-16 18:23:48 +0530

    Print from 1 to n & n to 1

--- Recursion questions/src/Main.java
+++ Recursion questions/src/Main.java
@@ -1,5 +1,27 @@
 public class Main {
     public static void main(String[] args) {
-
+        funRev(5);
     }
+
+    // function to print from n to 1
+    static void fun(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        fun(n - 1);
+    }
+
+
+    // Function to print from 1 to n
+    static void funRev(int n) {
+        if (n == 0) {
+            return;
+        }
+        funRev(n - 1);
+        System.out.println(n);
+    }
+
+}
+
 }
