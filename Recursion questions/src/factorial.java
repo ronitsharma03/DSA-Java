77ff8834ba2b4f248b9ee25c7fb179c29f4c9d8d
ronitsharma03 <ronitkhajuria03>
2024-03-16 18:49:40 +0530

    Sum of n to 1

--- Recursion questions/src/factorial.java
+++ Recursion questions/src/factorial.java
@@ -2,13 +2,11 @@ public class factorial {
     public static void main(String[] args) {
         System.out.println(fact(5));
     }
-    static int ans = 1;
     static int fact(int n){
 
         if(n <= 2){
             return n;
         }
-        ans = n * fact(n - 1);
-        return ans;
+        return n * fact(n - 1);
     }
 }
