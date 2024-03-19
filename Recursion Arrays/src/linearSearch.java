82df0c02f9699af63c5d2fe41fd5095833a66dff
ronitsharma03 <ronitkhajuria03>
2024-03-19 12:22:20 +0530

    modified linearSearch.java

--- Recursion Arrays/src/linearSearch.java
+++ Recursion Arrays/src/linearSearch.java
@@ -1,7 +1,11 @@
+import java.util.ArrayList;
+
 public class linearSearch {
     public static void main(String[] args) {
-        int[] arr = {1, 4, 23, 5, 2, 55, 7, 876};
-        System.out.println(linearSearch(arr, 0, 876));
+        int[] arr = {1, 4, 23, 5, 2, 55, 5, 7, 876};
+        System.out.println(linearSearch(arr, 0, 5));
+        ArrayList<Integer> ans = findAllIndex(arr, 0, 5, new ArrayList<>());
+        System.out.println(ans);
     }
 
     static int linearSearch(int[] arr, int index, int target){
@@ -13,5 +17,15 @@ public class linearSearch {
             return index;
         }
         return linearSearch(arr, index + 1, target);
+    }
+
+    static ArrayList<Integer> findAllIndex(int[] arr, int index, int target, ArrayList<Integer> list){
+        if(index == arr.length){
+            return list;
+        }
+        if(arr[index] == target){
+            list.add(index);
+        }
+        return findAllIndex(arr, index + 1, target, list);
     }
 }
