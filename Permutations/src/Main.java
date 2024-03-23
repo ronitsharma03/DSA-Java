0657bd88f7e77814301650de78aa1a6cb97b2665
ronitsharma03 <ronitkhajuria03>
2024-03-23 12:56:03 +0530

    Added code for string permutations with Arraylist

--- Permutations/src/Main.java
+++ Permutations/src/Main.java
@@ -1,6 +1,9 @@
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        permutations("", "abc");
+        System.out.println(permutationsList("", "abc"));
     }
 
     static void permutations(String p, String up){
@@ -15,5 +18,21 @@ public class Main {
             String s = p.substring(i, p.length());
             permutations(f+ch+s, up.substring(1));
         }
+    }
+
+    static ArrayList<String> permutationsList(String p, String up){
+        if(up.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        char ch = up.charAt(0);
+        ArrayList<String> ans = new ArrayList<>();
+        for (int i = 0; i <= p.length(); i++) {
+            String f = p.substring(0, i);
+            String s = p.substring(i, p.length());
+            ans.addAll(permutationsList(f+ch+s, up.substring(1)));
+        }
+        return ans;
     }
 }
