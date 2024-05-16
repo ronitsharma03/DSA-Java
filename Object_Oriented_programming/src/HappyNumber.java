4f588c9ca8e2d363f75c22fade9b6ce7260acb8a
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-05-16 20:21:06 +0530

    Leetcode - 202 (Happy Number)

---
+++ Object_Oriented_programming/src/HappyNumber.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/happy-number
+public class HappyNumber {
+    public static void main(String[] args) {
+        System.out.println(isHappy(12));
+    }
+
+    public static boolean isHappy(int n){
+        int slow = n;
+        int fast = n;
+
+        do{
+            slow = findSquare(slow);
+            fast = findSquare(findSquare(fast));
+        }while(slow != fast);
+
+        return slow == 1;
+    }
+
+    public static int findSquare(int num){
+        int ans = 0;
+        while(num > 0){
+            int rem = num % 10;
+            ans += rem * rem;
+            num /= 10;
+        }
+        return ans;
+    }
+}
