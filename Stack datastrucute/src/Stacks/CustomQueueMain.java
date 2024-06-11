f9bfab85edd877455a493bb9e9f988573220ff3b
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-11 16:36:07 +0530

    Circular queue implementation

--- Stack datastrucute/src/Stacks/CustomQueueMain.java
+++ Stack datastrucute/src/Stacks/CustomQueueMain.java
@@ -2,7 +2,7 @@ package Stacks;
 
 public class CustomQueueMain {
     public static void main(String[] args) throws Exception{
-        CustomQueue queue = new CustomQueue(5);
+        CircularQueue queue = new CircularQueue(5);
         queue.insert(1);
         queue.insert(2);
         queue.insert(3);
@@ -11,6 +11,7 @@ public class CustomQueueMain {
         queue.display();
 
         System.out.println(queue.remove());
+        queue.insert(122);
 
         queue.display();
     }
