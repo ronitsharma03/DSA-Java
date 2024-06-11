955974c6dfe270650da3ba33395a7523d6805111
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-11 10:13:08 +0530

    Queue implementation exampl

--- Stack datastrucute/src/Stacks/InbuiltExample.java
+++ Stack datastrucute/src/Stacks/InbuiltExample.java
@@ -1,20 +1,37 @@
 package Stacks;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 
 public class InbuiltExample {
     public static void main(String[] args) {
-        Stack<Integer> stack = new Stack<>();
-        stack.push(5);
-        stack.push(4);
-        stack.push(3);
-        stack.push(2);
-        stack.push(1);
+//        Stack<Integer> stack = new Stack<>();
+//        stack.push(5);
+//        stack.push(4);
+//        stack.push(3);
+//        stack.push(2);
+//        stack.push(1);
+//
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
+//        System.out.println(stack.pop());
 
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
-        System.out.println(stack.pop());
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(2);
+        queue.add(3);
+        queue.add(4);
+        queue.add(5);
+        queue.add(6);
+
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+        System.out.println(queue.remove());
+//        System.out.println(queue.remove());  Gives exception if the queue is already empty
     }
 }
