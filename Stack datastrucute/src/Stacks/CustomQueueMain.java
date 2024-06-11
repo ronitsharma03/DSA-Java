492d1a24383c04ab76fc2a055be7a6db3908c599
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-11 16:14:43 +0530

    Custom Queue implementation and methods

---
+++ Stack datastrucute/src/Stacks/CustomQueueMain.java
@@ -0,0 +1,17 @@
+package Stacks;
+
+public class CustomQueueMain {
+    public static void main(String[] args) throws Exception{
+        CustomQueue queue = new CustomQueue(5);
+        queue.insert(1);
+        queue.insert(2);
+        queue.insert(3);
+        queue.insert(4);
+        queue.insert(5);
+        queue.display();
+
+        System.out.println(queue.remove());
+
+        queue.display();
+    }
+}
