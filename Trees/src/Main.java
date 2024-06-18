e7424380a6bbffc202144bbbe5fc42ddb1d41494
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-18 16:25:21 +0530

    AVL trees

--- Trees/src/Main.java
+++ Trees/src/Main.java
@@ -7,9 +7,16 @@ public class Main {
 //        tree.populate(scanner);
 //        tree.prettyDisplay();
 
-        BST tree = new BST();
-        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-        tree.populateSorted(nums);
-        tree.display();
+//        BST tree = new BST();
+//        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+//        tree.populateSorted(nums);
+//        tree.display();
+
+        AVLTREE tree = new AVLTREE();
+        for (int i = 0; i < 1000; i++) {
+            tree.insert(i);
+        }
+        System.out.println(tree.height());
+//        tree.prettyTreeDisplay();
     }
 }
