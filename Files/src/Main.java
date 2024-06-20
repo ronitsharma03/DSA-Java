a82121672afea18592244adc01322c2e8c64a7d9
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-20 10:16:12 +0530

    File reading, creating, writting  and deleting

--- Files/src/Main.java
+++ Files/src/Main.java
@@ -28,12 +28,55 @@ public class Main {
 //            System.out.println(e.getMessage());
 //        }
 
-        try(BufferedReader fr = new BufferedReader(new InputStreamReader(System.in)))
+//        try(BufferedReader fr = new BufferedReader(new InputStreamReader(System.in)))
+//        {
+//            System.out.println("You typed: " + fr.readLine());
+//            // fr.close() automatically by try catch
+//        }catch (IOException e){
+//            System.out.println(e.getMessage());
+//        }
+
+
+//        // creates new filw
+//        try
+//        {
+//           File fo = new File("new-file.txt");
+//           fo.createNewFile();
+//        }catch (IOException e){
+//            System.out.println(e.getMessage());
+//        }
+
+        // write in the file
+//        try
+//        {
+//            FileWriter fw = new FileWriter("new-file.txt");
+//            fw.write("सर्वधर्मान्परित्यज्य मामेकं शरणं व्रज। अहं त्वा सर्वपापेभ्यो मोक्षयिष्यामि मा शुचः।।");
+//            fw.close();
+//        }catch (IOException e){
+//            System.out.println(e.getMessage());
+//        }
+
+        // Reading from the file
+        try(BufferedReader fr = new BufferedReader(new FileReader("new-file.txt")))
         {
-            System.out.println("You typed: " + fr.readLine());
-            // fr.close() automatically by try catch
+//            int letters = fr.read();
+//            while(fr.ready()){
+//                System.out.println((char)letters);
+//                letters = fr.read();
+//            }
+            while(fr.ready()){
+            System.out.println(fr.readLine());
+            }
         }catch (IOException e){
             System.out.println(e.getMessage());
         }
+
+
+            File fo = new File("note.txt");
+            if (fo.delete()){
+                System.out.println("Deleted " + fo.getName());
+            }
+
+
     }
 }
