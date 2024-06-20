897a1bca83567c472210b18fcdf2faf5feade3d0
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-20 09:50:58 +0530

    FileReader and BufferReader

--- Files/src/Main.java
+++ Files/src/Main.java
@@ -1,17 +1,38 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        try(InputStreamReader isr = new InputStreamReader(System.in)){
-            System.out.print("Enter some letters: ");
-            int letters = isr.read();
-            while(isr.ready()){
-                System.out.println((char) letters);
-                letters = isr.read();
-            }
+//        try(InputStreamReader isr = new InputStreamReader(System.in)){
+//            System.out.print("Enter some letters: ");
+//
+//            // the return type of read() is int so it returns the unicode of the char
+//            int letters = isr.read();
+//            while(isr.ready()){
+//                System.out.println((char) letters);
+//                letters = isr.read();
+//            }
+//
+//        }catch(IOException e){
+//            System.out.println(e.getMessage());
+//        }
 
-        }catch(IOException e){
+//        try(FileReader fr = new FileReader("note.txt"))
+//        {
+//            int letters = fr.read();
+//            while(fr.ready()){
+//                System.out.println((char)letters);
+//                letters = fr.read();
+//            }
+//            // fr.close() automatically by try catch
+//        }catch (IOException e){
+//            System.out.println(e.getMessage());
+//        }
+
+        try(BufferedReader fr = new BufferedReader(new InputStreamReader(System.in)))
+        {
+            System.out.println("You typed: " + fr.readLine());
+            // fr.close() automatically by try catch
+        }catch (IOException e){
             System.out.println(e.getMessage());
         }
     }
