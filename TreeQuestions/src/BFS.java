daff910c70acec60eaa5338ae2bf9921e8b4b514
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-23 10:36:15 +0530

    Bottomup left to right traversal

--- TreeQuestions/src/BFS.java
+++ TreeQuestions/src/BFS.java
@@ -1,10 +1,7 @@
 
 import com.sun.source.tree.Tree;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class BFS {
     public List<List<Integer>> levelorder(TreeNode root){
@@ -36,6 +33,36 @@ public class BFS {
         return result;
     }
 
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if(root == null){
+            return result;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode currentNode = queue.poll();
+                currentLevel.add(currentNode.val);
+                if(currentNode.left != null){
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right != null){
+                    queue.offer(currentNode.right);
+                }
+            }
+            // list has a index specifying method so add it in front everytime
+            result.add(0, currentLevel);
+        }
+
+        return result;
+    }
+
     public TreeNode findSuccessor(TreeNode root, int key){
         if(root == null){
             return null;
@@ -51,6 +78,39 @@ public class BFS {
             if(currNode.val == key) break;
         }
         return queue.poll();
+    }
+
+
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> list = new ArrayList<>();
+        if(root == null){
+            return list;
+        }
+
+        Deque<TreeNode> deq = new LinkedList<>();
+        boolean reverse = false;
+        deq.offer(root);
+
+        while (!deq.isEmpty()) {
+            int levelSize = deq.size();
+            List<Integer> currentLevel = new ArrayList<>();
+            for (int i = 0; i < levelSize; i++) {
+                if(!reverse) {
+                    TreeNode currentNode = deq.pollFirst();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.left != null) deq.addLast(currentNode.left);
+                    if (currentNode.right != null) deq.addLast(currentNode.right);
+                }else{
+                    TreeNode currentNode = deq.pollLast();
+                    currentLevel.add(currentNode.val);
+                    if (currentNode.left != null) deq.addFirst(currentNode.left);
+                    if (currentNode.right != null) deq.addFirst(currentNode.right);
+                }
+            }
+            list.add(currentLevel);
+        }
+        return list;
     }
 
     public class TreeNode{
