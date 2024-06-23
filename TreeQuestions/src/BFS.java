cdfe750a206bc4b14ace2c58b0e99c141f3f2a84
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-06-23 09:44:54 +0530

    LevelOrder successor of the given key Node

--- TreeQuestions/src/BFS.java
+++ TreeQuestions/src/BFS.java
@@ -36,6 +36,23 @@ public class BFS {
         return result;
     }
 
+    public TreeNode findSuccessor(TreeNode root, int key){
+        if(root == null){
+            return null;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode currNode = queue.poll();
+            if(currNode.left != null) queue.offer(currNode.left);
+            if(currNode.right != null) queue.offer(currNode.right);
+            if(currNode.val == key) break;
+        }
+        return queue.poll();
+    }
+
     public class TreeNode{
         int val;
         TreeNode left;
