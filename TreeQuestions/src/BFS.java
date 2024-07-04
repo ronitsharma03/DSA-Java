fc90a350a4b1956d5580662f619f6989cb8997bb
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-07-04 12:18:59 +0530

    Leetcode 230 - kth smallest node

--- TreeQuestions/src/BFS.java
+++ TreeQuestions/src/BFS.java
@@ -255,6 +255,32 @@ public class BFS {
         return root;
     }
 
+    //    230 - > https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+
+    int count = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        return helper(root, k).val;
+    }
+
+    public TreeNode helper(TreeNode node, int k){
+        if(node == null){
+            return null;
+        }
+
+        TreeNode left = helper(node.left, k);
+
+        if(left != null){
+            return left;
+        }
+        count++;
+        if(count == k){
+            return node;
+        }
+
+        return helper(node.right, k);
+    }
+
+
     //    236 -> https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         if(root == null){
