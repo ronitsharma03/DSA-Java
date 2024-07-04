acd76ac7d848421539dc76dd855c4106dd2cda50
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-07-04 11:17:58 +0530

    Leetcode 236 - lowest common ancestor

--- TreeQuestions/src/BFS.java
+++ TreeQuestions/src/BFS.java
@@ -255,6 +255,21 @@ public class BFS {
         return root;
     }
 
+    //    236 -> https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null){
+            return null;
+        }
+
+        if(root == p || root == q){
+            return root;
+        }
+
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+
+        return left == null ? right : left;
+    }
 
     // 543 -> https://leetcode.com/problems/diameter-of-binary-tree/description/
     int diameter = 0;
