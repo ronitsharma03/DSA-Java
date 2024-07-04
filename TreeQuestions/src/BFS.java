8620e7719a5ba95fcbfcf6d74d069f78b4ff80fd
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-07-04 13:44:34 +0530

    Leetcode 105 - constructing the tree from the preorder and inorder

--- TreeQuestions/src/BFS.java
+++ TreeQuestions/src/BFS.java
@@ -280,6 +280,28 @@ public class BFS {
         return helper(node.right, k);
     }
 
+    //    105 -> https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/description/
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder.length == 0){
+            return null;
+        }
+
+        int root = preorder[0];
+        int index = 0;
+
+        for (int i = 0; i < inorder.length; i++) {
+            if(inorder[i] == root){
+                index = i;
+            }
+        }
+
+        TreeNode node = new TreeNode(root);
+
+        node.left = buildTree(Arrays.copyOfRange(preorder, 1, index+1), Arrays.copyOfRange(inorder, 0, index));
+        node.right = buildTree(Arrays.copyOfRange(preorder, index+1, preorder.length), Arrays.copyOfRange(inorder, index+1, inorder.length));
+        return node;
+    }
+
 
     //    236 -> https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
