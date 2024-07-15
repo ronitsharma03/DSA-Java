afa2034ce1161c669f07d60e992373462c978ecb
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-07-15 11:31:27 +0530

    Removal from heap data structures

--- HeapDS/src/heaps.java
+++ HeapDS/src/heaps.java
@@ -49,6 +49,51 @@ public class heaps<T extends Comparable<T>> {
         }
     }
 
+    private T remove() throws Exception{
+        // If the heap is empty throws an exception
+        if(list.isEmpty()){
+            throw new Exception("Removing from the empty heap!");
+        }
 
+        // Get the root element because that is going to be removed
+        T removed = list.get(0);
+        // Now as the root is empty we have to put the last element at the root position
+        T last = list.remove(list.size() - 1);
+        if(!list.isEmpty()){
+            list.set(0, last);
+            // while insertion we go and swap upwards as the insertion takes place from the last index and we compare upwards but while removal we remove from top and insert the last index node at the very first place so we now compare them downwards -> downheap
+            downheap(0);
+        }
+
+
+        return removed;
+    }
+
+    private void downheap(int index) {
+        // Assume the minimum to be the actual current index
+        int min = index;
+
+        // find the left and the right index child
+        int left = left(index);
+        int right = right(index);
+
+        // if the min is greater than its left child then make the left child new min
+        if(left < list.size() && list.get(min).compareTo(list.get(left)) > 0){
+            min = left;
+        }
+
+        // if the min is greater than its right child then make the right child new min
+        if(right < list.size() && list.get(min).compareTo(list.get(right)) > 0){
+            min = right;
+        }
+
+        // now check if the min is not equal to index the actual one -> then it means the new min is either left or right then just simply swap the new min and the index value and call the downheap to the swapped node at min
+        if(min != index){
+            swap(min, index);
+            downheap(min);
+        }
+        // if the min is equal to the index it means the tree is just fine it dont need any further operations
+
+    }
 
 }
