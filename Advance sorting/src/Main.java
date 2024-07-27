a2311fca2381925de13c0addcd914e1af8e0f589
ronitsharma03 <ronitkhajuria03@gmail.com>
2024-07-27 16:47:19 +0530

    Advance sorting - counting sort using hashmap

--- Advance sorting/src/Main.java
+++ Advance sorting/src/Main.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
@@ -32,6 +34,28 @@ public class Main {
                 arr[index] = i;
                 index++;
                 freqArray[i]--;
+            }
+        }
+    }
+
+    public static void countSortHash(int[] arr){
+        if(arr == null || arr.length <= 1){
+            return;
+        }
+        int max = Arrays.stream(arr).max().getAsInt();
+        int min = Arrays.stream(arr).min().getAsInt();
+
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        for(int num: arr){
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+        int index = 0;
+        for (int i = min; i <= max; i++) {
+            int count = countMap.getOrDefault(i, 0);
+            for (int j = 0; j < count; j++) {
+                arr[index] = i;
+                index++;
             }
         }
     }
